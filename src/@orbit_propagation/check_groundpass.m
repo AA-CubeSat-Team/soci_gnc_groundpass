33485f0f00643baddc149df00cae06df70298f67
Taylor Reynolds <tpr6@uw.edu>
2020-04-22 13:19:30 -0700

    fixed the elevation calculation to have the correct sign, verified that the slant range is printed out starting at elevation zero

--- src/@orbit_propagation/check_groundpass.m
+++ src/@orbit_propagation/check_groundpass.m
@@ -4,19 +4,20 @@ err_ecef_m  = obj.pos_ecef_m - obj.gs.pos_ecef_m;
 err_sez_m   = rot(90-obj.gs.lat_deg,'y') * rot(obj.gs.lon_deg,'z') * err_ecef_m;
 err_mag_m   = norm(err_sez_m);
 s_ell       = err_sez_m(3)/err_mag_m;
-c_ell       = norm(err_sez_m(1:2))/err_mag_m;
-ell         = acosd(c_ell);
+% c_ell       = norm(err_sez_m(1:2))/err_mag_m;
+ell         = atan2d( err_sez_m(3), norm(err_sez_m(1:2)) );
 
 if (obj.logging)
     fprintf(obj.log_file_id,obj.log_file_spec(1,3),...
-                                            ell,err_sez_m(3)*obj.params.m2km);
+                                         ell,err_sez_m(3)*obj.params.m2km);
 end
 
 % check if we are overhead
 if (s_ell>=0)
     % print current slant range to log file
     if (obj.logging)
-        fprintf(obj.log_file_id,obj.log_file_spec(1,4),err_mag_m*obj.params.m2km);
+        fprintf(obj.log_file_id,obj.log_file_spec(1,4),...
+                                        err_mag_m*obj.params.m2km);
     end
     % starting or ending a pass
     if (abs(ell)>(min_elevation) && ~obj.passing)
